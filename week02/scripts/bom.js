ac236d55af24bcea573f8b89a54d263f5a45532b
ismaelmarin2010 <65473600+ismaelmarin2010@users.noreply.github.com>
2026-03-17 02:21:43 -0300

    W02 Assignment: Picture Album

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -8,11 +8,10 @@ button.addEventListener('click', function () {
     // Code to execute when the button is clicked
     if (input.value.trim() !== '') {
 
-        const li = document.createElement('li');
+        const li = document.createElement('li'); // Creates new line (list item)
+        const deleteButton = document.createElement('button'); // Creates the delete button next to the new line added
 
-        const deleteButton = document.createElement('button');
-
-        const text = document.createTextNode(input.value);
+        const text = document.createTextNode(input.value); // Could not make it work until I replaced the way the input is taken.
 
         li.appendChild(text);
         deleteButton.textContent = '❌';
@@ -25,9 +24,8 @@ button.addEventListener('click', function () {
             input.focus();
         });
 
-        input.value = '';
-
-        input.focus();
+        input.value = ''; // Clears the input field
+        input.focus(); // Takes the focus back to the input
 
     };
 });
